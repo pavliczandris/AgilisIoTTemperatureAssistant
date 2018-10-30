c549e5638e4d51891e9a0c36de86ac1e054130bf
szbence26 <szokebenc@gmail.com>
2018-10-30 21:59:45 +0100

    új modell létrehozása és file kezelő service írása

---
+++ IoTTemperatureAssistant/IoTTemperatureAssistant/Models/TempData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTTemperatureAssistant.Models
+{
+    public class TempData
+    {
+        public DateTime Time { get; set; }
+        public double Temperature { get; set; }
+
+        public TempData()
+        {
+
+        }
+
+        public TempData(DateTime time, double temperature)
+        {
+            Time = time;
+            Temperature = temperature;
+        }
+
+        public TempData(string time, string temperature)
+        {
+            Time = new DateTime(long.Parse(time));
+            Temperature = double.Parse(temperature);
+        }
+    }
+}
---
+++ IoTTemperatureAssistant/IoTTemperatureAssistant/Services/FileService.cs
@@ -0,0 +1,59 @@
+﻿using IoTTemperatureAssistant.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IoTTemperatureAssistant.Services
+{
+    public class FileService
+    {
+        public void SaveToFile(string fileName, List<TempData> dataList)
+        {
+            fileName = fileName.Split('.')[0];
+            string filePath = $"{fileName}.csv";
+            string[] lines = new string[dataList.Count];
+            for (int i = 0; i < dataList.Count; i++)
+            {
+                lines[i] = $"{dataList[i].Time.Ticks};{dataList[i].Temperature}";
+            }
+            File.WriteAllLines(filePath, lines);
+        }
+
+        public List<TempData> LoadFromFile(string filePath)
+        {
+            string[] lines;
+            var dataList = new List<TempData>();
+            try
+            {
+                lines = File.ReadAllLines(filePath);
+            }
+            catch (FileNotFoundException)
+            {
+                return dataList;
+            }
+            foreach (var line in lines)
+            {
+                var values = line.Split(';');
+                if(values.Length == 2)
+                    dataList.Add(new TempData(values[0], values[1]));
+            }
+            return dataList;
+        }
+
+        public string CreateDateString(DateTime date, string separator = "-")
+        {
+            string month = "";
+            string day = "";
+
+            if (date.Month < 10)
+                month = $"0{date.Month}";
+            else month = date.Month.ToString();
+
+            if (date.Day < 10)
+                day = $"0{date.Day}";
+            else day = date.Day.ToString();
+
+            return $"{date.Year}{separator}{month}{separator}{day}";
+        }
+    }
+}
