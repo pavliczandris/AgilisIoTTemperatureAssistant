c5e48776b98d09338e1915cc5be30b48215327f2
Bogár György Richárd <43424155+bogarrichard@users.noreply.github.com>
2018-11-22 16:34:22 +0100

    TemperatureCalculator class and Tests

---
+++ IoTTemperatureAssistant/IoTTemperatureAssistant/TemperatureCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTTemperatureAssistant
+{
+    public class TemperatureCalculator
+    {
+
+        private double volumeOfRoom         { get; set; }
+        private double enerGyPricing        { get; set; }
+        private double outdoorTemperature   { get; set; }
+        private double indoorTemperature    { get; set; }
+        private double goodnessFactor       { get; set; }
+
+        public double TemperatureDifference()   // Belső és külső hőmérséklet közti különbség számítása
+        {
+            return 0;
+        }
+
+        public double EnergyToMaintain()        // Jelenlegi hőmérséklet fenntartásához szükséges energia, arra számítva, hogy a hőmérséklet azonos marad a jelenlegivel
+        {
+            return 0;
+        }
+
+        public double EnergyToChange(double valueofChange)  // Hőmérséklet megváltoztatásához szükséges energia
+        {
+            return 0;
+        }
+
+        public double PriceToMaintain()         // Jelenlegi hőmérséklet fenntartásából adódó költség a hónapra
+        {
+            return 0;
+        } 
+
+        public double PriceToChange(double valueOfChange)           // A hőmérsékletváltoztatásából adódó költség egyszeri hőmérsékletváltoztatásnál.
+        {
+            return 0;
+        }
+    }
+}
--- IoTTemperatureAssistant/TestProject/UnitTest1.cs
+++ IoTTemperatureAssistant/TestProject/UnitTest1.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IoTTemperatureAssistant;
 
 namespace TestProject
 {
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void TempDiffTests() // Hőmérsékletkülönbség számításához teszt.
         {
+            double expectedDiff =0;
+            TemperatureCalculator tc = new TemperatureCalculator();
+            Assert.AreEqual(expectedDiff, tc.TemperatureDifference());
+        }
+
+        [TestMethod]
+        public void MaintainTests() // Tesztek a fenntartáshoz szükséges energia és költség számításához
+        {
+            double expectedEnergy = 0;
+            double expectedPrice = 0;
+            TemperatureCalculator tc = new TemperatureCalculator();
+            Assert.AreEqual(expectedEnergy,tc.EnergyToMaintain());
+            Assert.AreEqual(expectedPrice,tc.PriceToMaintain());
+        }
+
+        [TestMethod]
+        public void ChangeAndMaintainTests()   // Tesztek a hőmérséklet változtatásához és megtartásához szükséges energia és költség számításáshoz
+        {
+            double expectedEnergy = 0;
+            double expectedPrice = 0;
+            TemperatureCalculator tc = new TemperatureCalculator();
+            Assert.AreEqual(expectedEnergy, tc.EnergyToChange(0));
+            Assert.AreEqual(expectedPrice, tc.PriceToChange(0));
         }
     }
 }
