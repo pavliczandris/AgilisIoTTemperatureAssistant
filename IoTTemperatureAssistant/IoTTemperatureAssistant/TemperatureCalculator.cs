c7eec5a367fa830abf61321d19535ef6baf310e2
hodosykristof <44854659+hodosykristof@users.noreply.github.com>
2018-11-23 00:33:59 +0100

    chicken nugget test

--- IoTTemperatureAssistant/IoTTemperatureAssistant/TemperatureCalculator.cs
+++ IoTTemperatureAssistant/IoTTemperatureAssistant/TemperatureCalculator.cs
@@ -9,16 +9,16 @@ namespace IoTTemperatureAssistant
     public class TemperatureCalculator
     {
         private double volumeOfRoom         { get; set; }   // Szoba térfogata
-        private double enerGyPricing        { get; set; }   // Energia árfolyama
+        private double energyPricing        { get; set; }   // Energia árfolyama
         private double outdoorTemperature   { get; set; }   // Külső hőmérséklet
-        private double indoorTemperature    { get; set; }   //Belső hőmérséklet
-        private double goodnessFactor       { get; set; }   // Jóségi tényező a bel- és kültér közötti fal szigetelésére. 1=teljesen szigetel; 0=teljesen átadja a hőt
+        private double indoorTemperature    { get; set; }   // Belső hőmérséklet
+        private double goodnessFactor       { get; set; }   // Jósági tényező a bel- és kültér közötti fal szigetelésére. 1=teljesen szigetel; 0=teljesen átadja a hőt
         private double surfaceOfWall        { get; set; }   // Bel, és külteret elválasztó falak összfelülete
 
         public TemperatureCalculator()
         {
             volumeOfRoom = 0;
-            enerGyPricing = 0;
+            energyPricing = 0;
             outdoorTemperature = 0;
             indoorTemperature = 0;
             goodnessFactor = 1;
